86b62e2f5e722cc413d132e3d36153ed6c36e337
Mirandad10 <dmiranda0005@gmail.com>
2023-11-20 15:19:10 -0800

    added/finished code for config and controllers

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.update(req.body, {
+            where: {
+                id: req.params.id,
+            }
+        });
+        return res.json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const delComment = await Comment.destroy({ where: { id: req.params.id } });
+        res.status(200).json(delComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            }
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const delPost = await Post.destroy({ where: { id: req.params.id } });
+        if (!delPost) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json(delPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
