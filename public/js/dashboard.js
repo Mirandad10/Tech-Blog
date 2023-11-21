10e351f381fea7f02f3d997e0fb14c76eba327ec
Mirandad10 <dmiranda0005@gmail.com>
2023-11-20 16:17:12 -0800

    added and finished all js files with styling

---
+++ public/js/dashboard.js
@@ -0,0 +1,39 @@
+const showButton = document.querySelector('#show-create');
+const newPost = document.querySelector('#new-post');
+
+const postFormHandler = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#new-title').value.trim();
+    const text = document.querySelector('#new-content').value.trim();
+    if (title && text) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, text }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+const toggleCreate = (event) => {
+    event.preventDefault();
+    if (showButton.dataset.shown == 'true') {
+        newPost.reset();
+        newPost.classList.add('d-none');
+        document.querySelector('#posts').classList.remove('d-none');
+        showButton.textContent = '+ New Post';
+        showButton.dataset.shown = 'false';
+    } else {
+        newPost.classList.remove('d-none');
+        document.querySelector('#posts').classList.add('d-none');
+        showButton.textContent = 'Cancel';
+        showButton.dataset.shown = 'true';
+    }
+}
+
+newPost.addEventListener('submit', postFormHandler);
+showButton.addEventListener('click', toggleCreate);
---
+++ public/js/login.js
@@ -0,0 +1,54 @@
+const loginForm = document.querySelector('#login-form');
+const switcher = document.querySelector('#login-toggle');
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+    if (name && password) {
+        if (loginForm.dataset.login == 'true') {
+            const response = await fetch('/api/users/login', {
+                method: 'POST',
+                body: JSON.stringify({ name, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (response.ok) {
+
+                document.location.replace('/');
+            } else {
+                alert(response.statusText);
+            }
+        } else {
+            const response = await fetch('/api/users', {
+                method: 'POST',
+                body: JSON.stringify({ name, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (response.ok) {
+
+                document.location.replace('/');
+            } else {
+                alert(response.statusText);
+            }
+        };
+    }
+};
+
+const logOrSign = (event) => {
+    event.preventDefault();
+    if (loginForm.dataset.login == 'true') {
+        loginForm.dataset.login = false;
+        document.querySelector('#login-header').textContent = 'Sign Up';
+        document.querySelector('#loginButton').textContent = 'Sign Up';
+        switcher.textContent = 'Login instead';
+    } else {
+        loginForm.dataset.login = true;
+        document.querySelector('#login-header').textContent = 'Login';
+        document.querySelector('#loginButton').textContent = 'Login';
+        switcher.textContent = 'Sign up instead';
+    }
+}
+
+loginForm.addEventListener('submit', loginFormHandler);
+switcher.addEventListener('click', logOrSign);
